2eb5e682218d563ef6d7d21a759a7eb91d77b89d
soyhyoj <soyhyoj@gmail.com>
2021-02-16 14:54:57 +0100

    na, numerical, categorical variables inspection + correlation with isFraud

--- Fraud Detection in R/EDA in R.R
+++ Fraud Detection in R/EDA in R.R
@@ -14,13 +14,34 @@ summary(paysim)
 # or use one of many libraries like dplyr
 
 
+######################################################################################
 # Import skimr packages to display additional summary info including missing values
 library(skimr)
-skim(paysim)
+skimr::skim(paysim) # applying skim function of skimr package
 
-#
+# Count missing values and plot the result
 library(inspectdf)
-inspect_na(paysim)
+library(dplyr)
+show_plot(inspect_na(paysim))
 
 # Numeric variables inspection
-inspect_num(paysim, show_plot = TRUE)
+show_plot(inspect_num(paysim))
+# show_plot argument not working... Display histrograms here
+
+# Categorical variable
+inspect_imb(paysim)
+inspect_cat(paysim)
+
+######################################################################################
+# How many fraudulent transactions(= our target) are there? 
+df_isFraud <- dplyr::filter(paysim, isFraud == 1)
+df_notFraud <- dplyr::filter(paysim, isFraud == 0)
+show_plot(inspect_mem(df_isFraud, df_notFraud))
+show_plot(inspect_cat(df_isFraud, df_notFraud))
+######################################################################################
+# Correlation of columns
+show_plot(inspect_cor(df_isFraud))
+
+######################################################################################
+######################################################################################
+######################################################################################
